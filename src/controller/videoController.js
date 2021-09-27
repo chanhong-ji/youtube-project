827c99ac452973fbf709323ee64d2b940c491e8b
chanhong-ji <jichanhong24@gmail.com>
2021-09-27 22:26:35 +0900

    .
    postEdit

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -27,6 +27,10 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found" });
+  }
   await Video.findByIdAndUpdate(id, { title, description, hashtags });
   return res.redirect(`/videos/${id}`);
 };
