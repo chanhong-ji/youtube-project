aeea6b6830cb5428c333f83e1ec96a553064fa1a
chanhong-ji <jichanhong24@gmail.com>
2021-09-28 17:52:35 +0900

    Fix rootRouter
    Change name of global router to rootRouter

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalrouter";
+import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRrouter";
 import videoRouter from "./routers/videoRouter";
 
@@ -9,7 +9,7 @@ const app = express();
 app.set("views", process.cwd() + "/src/views/");
 app.set("view engine", "pug");
 app.use(express.urlencoded({ extended: true }));
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
