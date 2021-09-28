0ef7e3fc1ffe27af299272cfb186f4b8a9221dfd
chanhong-ji <jichanhong24@gmail.com>
2021-09-28 20:23:16 +0900

    Create join controller with bcript
    by using schema middleware, make password be hashed before being saved.

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,25 @@
-export const join = (req, res) => res.render("join");
+import User from "../model/User";
+
+export const getJoin = (req, res) =>
+  res.render("join", { pageTitle: "Create Account" });
+
+export const postJoin = async (req, res) => {
+  const { name, email, username, password, location } = req.body;
+  try {
+    await User.create({
+      name,
+      email,
+      username,
+      password,
+      location,
+    });
+  } catch (error) {
+    const errorMessage = error._message;
+    return res.render("join", { pageTitle: "Join", errorMessage });
+  }
+  return res.redirect("/");
+};
+
 export const login = (req, res) => res.send("user login");
 
 export const see = (req, res) => res.send("Edit User profile");
