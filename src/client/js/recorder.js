259b01bd6ea1a6ac2afc9c8af86347d1fd041e5e
chanhong-ji <jichanhong24@gmail.com>
2021-10-13 22:58:31 +0900

    Update recorder.js

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,49 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
+let stream;
+let recorder;
+let videoFile;
 
-const onStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
     video: true,
   });
-  console.log(stream);
+  video.srcObject = stream;
+  video.play();
+};
+
+const onDownload = () => {
+  startBtn.innerText = "Start Recording";
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording";
+  document.body.appendChild(a);
+  a.click();
+};
+
+const onStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", onStop);
+  startBtn.addEventListener("click", onDownload);
+  recorder.stop();
+};
+
+const onStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", onStart);
+  startBtn.addEventListener("click", onStop);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
 };
 
 startBtn.addEventListener("click", onStart);
+
+init();
