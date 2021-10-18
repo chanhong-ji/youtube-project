dca776a3c1a653a2dc3d210298b43fdc2905c232
chanhong-ji <jichanhong24@gmail.com>
2021-10-18 17:41:39 +0900

    Update commentSection.js
    with fake comment & status code from fetch

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -3,6 +3,19 @@ const form = document.getElementById("commentForm");
 const textarea = form.querySelector("textarea");
 const btn = form.querySelector("button");
 
+const addComment = (text) => {
+  const comments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const i = document.createElement("i");
+  i.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  newComment.appendChild(i);
+  newComment.appendChild(span);
+  comments.prepend(newComment);
+};
+
 const onSubmit = async (event) => {
   event.preventDefault();
   const text = textarea.value;
@@ -10,7 +23,7 @@ const onSubmit = async (event) => {
   if (text.trim() === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -18,7 +31,11 @@ const onSubmit = async (event) => {
     body: JSON.stringify({ text }),
   });
   textarea.value = "";
-  window.location.reload();
+  if (status === 201) {
+    addComment(text);
+  } else {
+    console.log("fail");
+  }
 };
 
 form.addEventListener("submit", onSubmit);
