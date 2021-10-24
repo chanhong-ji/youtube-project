cfd433168fafcca1b1d751faa501cd9007b279ae
chanhong-ji <jichanhong24@gmail.com>
2021-10-25 02:20:58 +0900

    Fix

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ mongoose.connect(process.env.DB_URL, {
 
 const db = mongoose.connection;
 const handleOpen = () => console.log("✅ Connected to DB");
-const handleError = () => console.log("💥 DB error", error);
+const handleError = (error) => console.log("💥 DB error", error);
 
 db.on("error", handleError);
 db.once("open", handleOpen);
